54eedfadf9379601f9dec25009a65596b6826f84
Alexandru Văleanu <valeanu_alex@yahoo.com>
2019-12-27 11:42:38 +0000

    Create test_simple.py

---
+++ tests/test_simple.py
@@ -0,0 +1,16 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+
+def test_success():
+    # Another useless comment...
+    assert True
+
+def test_success_2():
+    # Another useless comment...
+    assert True
+
+def test_success_3():
+    # Another useless comment...
+    assert True
